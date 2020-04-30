1fc2aaedb6409aff31aea28b14cf50edd1de829e
Nick <252nick@gmail.com>
2020-04-30 16:58:01 -0400

    Added plan for rnn script

---
+++ rnn.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#By Nick Serger
+
+"""
+# Project outline:
+    import functions
+    
+    load data
+    divide data into train, validation, test
+
+    build model
+    compile model
+
+    train model
+    validate model
+
+    predict next value
+
+    test model
+"""
+
+# TODO: Import functions
+
+# TODO: Build a load data function
+# TODO: Build a divide data function
+
+# TODO: Write a build model function
+# TODO: Write a compile model function
+
+# TODO: Write a train model function
+# TODO: Write a validate model function
+
+# TODO: Predict the next value given the input
+
+# TODO: Write an evaluate model function
